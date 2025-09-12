51d7a247dbc1fc5ba139f20db0a5ab2029a027a0
Dima <mdmitry2002@gmail.com>
2025-09-13 00:49:32 +0300

    feat(Infrastructure): add AppDbContext with DbSet and configurations

---
+++ src/backend/FitCoachPro.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FitCoachPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitCoachPro.Infrastructure.Persistence
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<WorkoutPlan> WorkoutPlans { get; set; }
+
+        public DbSet<WorkoutItem> WorkoutItems { get; set; }
+
+        public DbSet<Exercise> Exercises { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
