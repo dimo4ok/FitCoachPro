6cfd103b70664e0f4eab80dea3c6a066f1fbc7ea
Dima <mdmitry2002@gmail.com>
2025-09-13 00:24:48 +0300

    feat(Infrastructure): add EF Core configurations for all entities

---
+++ src/backend/FitCoachPro.Infrastructure/Persistence/EntityConfigurations/ExerciseEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using FitCoachPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Infrastructure.Persistence.EntityConfigurations
+{
+    public class ExerciseEntityConfiguration : IEntityTypeConfiguration<Exercise>
+    {
+        public void Configure(EntityTypeBuilder<Exercise> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.ExerciseName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.GifUrl)
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/backend/FitCoachPro.Infrastructure/Persistence/EntityConfigurations/UserEntityConfiguration.cs
@@ -0,0 +1,49 @@
+﻿using FitCoachPro.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Infrastructure.Persistence.EntityConfigurations
+{
+    public class UserEntityConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.FirstName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.LastName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.TelephoneNumber)
+                .HasMaxLength(20);
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired();
+
+            builder.Property(x => x.Role)
+                .IsRequired();
+
+            builder.Property(x => x.DateOfRegistration)
+                .IsRequired();
+
+            builder.Property(x => x.DateOfExpiredSubscription);
+
+            builder.HasMany(x => x.WorkoutPlans)
+                .WithOne(x => x.User)
+                .HasForeignKey(x => x.UserId);
+        }
+    }
+}
+
+
+
+
