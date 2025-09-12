15180db9d4b0eb6e949bd942b32dfbb1ac3c1f30
Dima <mdmitry2002@gmail.com>
2025-09-12 23:15:31 +0300

    feat(Domain): add UserRole enum

---
+++ src/backend/FitCoachPro.Domain/Entities/Enums/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Enums
+{
+    public enum UserRole
+    {
+        //Admin,
+        //User,
+        //Trainer
+        User
+    }
+}
