03930cb3b6bf59f96f941e229a3177bf46b8b979
Dima <mdmitry2002@gmail.com>
2025-09-12 22:45:31 +0300

    feat(Domain): add WorkoutPlan domain model

---
+++ src/backend/FitCoachPro.Domain/Entities/WorkoutPlan.cs
@@ -0,0 +1,12 @@
+﻿namespace FitCoachPro.Domain.Entities
+{
+    public class WorkoutPlan
+    {
+        public Guid Id { get; protected set; }
+        public DateTime DateOfDoing { get; private set; }
+
+        public Guid UserId { get; private set; }
+        public User User { get; private set; } = null!;
+        public ICollection<WorkoutItem> WorkoutItems { get; set; } = new List<WorkoutItem>();
+    }
+}
