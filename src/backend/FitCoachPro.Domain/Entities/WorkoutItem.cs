9b926ad3b5aac6307fefb4c8312571508cfa1d24
Dima <mdmitry2002@gmail.com>
2025-09-12 22:44:46 +0300

    feat(Domain): add WorkoutItem domain model

---
+++ src/backend/FitCoachPro.Domain/Entities/WorkoutItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities
+{
+    public class WorkoutItem
+    {
+        public Guid id {  get; set; }
+        public string Description { get; set; } = null!;
+
+        public Guid WorkoutPlanId { get; set; }
+        public WorkoutPlan WorkoutPlan { get; set; } = null!;
+
+        public Guid ExerciseId { get; set; }
+        public Exercise Exercise { get; set; } = null!;
+    }
+}
