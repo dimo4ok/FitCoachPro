b4d3ec4881ef597c8f74fc8e4578946e3879d266
Dima <mdmitry2002@gmail.com>
2025-09-13 20:22:04 +0300

    feat(Infrastructure): add AppDbContextFactory

---
+++ src/backend/FitCoachPro.Infrastructure/Persistence/AppDbContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Infrastructure.Persistence
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        { 
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            //var configuration = new ConfigurationBuilder()
+            //   .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..\\FitCoachPro.API"))
+            //     .AddJsonFile("appsettings.json", optional: false)
+            //     .AddJsonFile("appsettings.Development.json", optional: true)
+            //     .Build();
+
+            //var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=FitCoachProDb;Integrated Security=True;Connect Timeout=30;"); // can't run wit connectionString
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
