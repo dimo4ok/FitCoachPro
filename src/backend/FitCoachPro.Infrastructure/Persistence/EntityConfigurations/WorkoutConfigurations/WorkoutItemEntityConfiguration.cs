fb5b5e09c942fd373cd779a9f326dc71cc12df71
Dima <mdmitry2002@gmail.com>
2025-09-14 23:20:51 +0300

    feat(Infrastructure): add updated workout configuration for new entities

---
+++ src/backend/FitCoachPro.Infrastructure/Persistence/EntityConfigurations/WorkoutConfigurations/WorkoutItemEntityConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using FitCoachPro.Domain.Entities.Workouts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Infrastructure.Persistence.EntityConfigurations.WorkoutConfigurations
+{
+    public class WorkoutItemEntityConfiguration : IEntityTypeConfiguration<WorkoutItem>
+    {
+        public void Configure(EntityTypeBuilder<WorkoutItem> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.HasOne(x => x.WorkoutPlan)
+                .WithMany(x => x.WorkoutItems)
+                .HasForeignKey(x => x.WorkoutPlanId)
+                .IsRequired();
+
+            builder.HasOne(x => x.Exercise)
+                .WithMany()
+                .HasForeignKey(x => x.ExerciseId)
+                .IsRequired();
+        }
+    }
+}
