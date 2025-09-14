8e7fced191c2605a53387b462e45fb8cee4d12b7
Dima <mdmitry2002@gmail.com>
2025-09-14 23:17:41 +0300

    feat(Domain): introduce BaseWorkoutItem with WorkoutItem and TemplateWorkoutItem inheritance

---
+++ src/backend/FitCoachPro.Domain/Entities/Workouts/Exercise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Workouts
+{
+    public class Exercise
+    {
+        public Guid Id { get; set; }
+        public string ExerciseName { get; set; } = null!;
+        public string GifUrl { get; set; } = null!;
+    }
+}
---
+++ src/backend/FitCoachPro.Domain/Entities/Workouts/WorkoutItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Workouts
+{
+    public class WorkoutItem : BaseWorkoutItem
+    {
+        public Guid WorkoutPlanId { get; set; }
+        public WorkoutPlan WorkoutPlan { get; set; } = null!;
+    }
+}
