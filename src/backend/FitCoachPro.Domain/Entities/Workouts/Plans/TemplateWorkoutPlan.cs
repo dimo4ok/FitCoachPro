e6c252692df315e1a920df7b1e7d222cff1a3db0
Dima <mdmitry2002@gmail.com>
2025-09-15 11:05:04 +0300

    feat(Domain): add template workout plans

---
+++ src/backend/FitCoachPro.Domain/Entities/Workouts/Plans/TemplateWorkoutPlan.cs
@@ -0,0 +1,21 @@
+﻿using FitCoachPro.Domain.Entities.Users;
+using FitCoachPro.Domain.Entities.Workouts.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Workouts.Plans
+{
+    public class TemplateWorkoutPlan : BaseWorkoutPlan
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        public Guid CoachId { get; set; }
+        public Coach Coach { get; set; } = null!;
+
+        public ICollection<TemplateWorkoutItem> TemplateWorkoutItems { get; set; } = new List<TemplateWorkoutItem>();
+    }
+}
