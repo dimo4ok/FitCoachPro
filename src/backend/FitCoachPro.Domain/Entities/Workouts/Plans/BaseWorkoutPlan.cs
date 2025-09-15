fbea6ab331a6b2033cb421f3bfed67d8ec82aeb2
Dima <mdmitry2002@gmail.com>
2025-09-15 11:10:01 +0300

    feat(Domain): add BaseWorkoutPlan as parent class for workout plans

---
+++ src/backend/FitCoachPro.Domain/Entities/Workouts/Plans/BaseWorkoutPlan.cs
@@ -0,0 +1,14 @@
+﻿using FitCoachPro.Domain.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Workouts.Plans
+{
+    public abstract class BaseWorkoutPlan 
+    {
+        public Guid Id { get; set; }
+    }
+}
