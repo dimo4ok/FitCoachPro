fc5ad28900eeaf2460626f92cb5070c7447bf31c
Dima <mdmitry2002@gmail.com>
2025-09-15 11:12:00 +0300

    feat(Domain): update fields for workouts

---
+++ src/backend/FitCoachPro.Domain/Entities/Workouts/Items/TemplateWorkoutItem.cs
@@ -0,0 +1,16 @@
+﻿using FitCoachPro.Domain.Entities.Users;
+using FitCoachPro.Domain.Entities.Workouts.Plans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Workouts.Items
+{
+    public class TemplateWorkoutItem : BaseWorkoutItem
+    {
+        public Guid TemplateWorkoutPlanId { get; set; }
+        public TemplateWorkoutPlan TemplateWorkoutPlan { get; set; } = null!;
+    }
+}
---
+++ src/backend/FitCoachPro.Domain/Entities/Workouts/Items/WorkoutItem.cs
@@ -0,0 +1,15 @@
+﻿using FitCoachPro.Domain.Entities.Workouts.Plans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Domain.Entities.Workouts.Items
+{
+    public class WorkoutItem : BaseWorkoutItem
+    {
+        public Guid WorkoutPlanId { get; set; }
+        public WorkoutPlan WorkoutPlan { get; set; } = null!;
+    }
+}
