1da854f7c46e2da0eda5888ab84a3569f1ba737a
Dima <mdmitry2002@gmail.com>
2025-09-16 13:43:54 +0300

    feat(Infrastructure): ApplicationUser entity for Identity integration

---
+++ src/backend/FitCoachPro.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using FitCoachPro.Domain.Entities.Users;
+using FitCoachPro.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public Guid DomainUserId { get; set; }
+        public User DomainUser { get; set; } = null!;
+    }
+}
