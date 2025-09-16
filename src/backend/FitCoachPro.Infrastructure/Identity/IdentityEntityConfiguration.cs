76f2a97d4199d02057c7d5f003719972558b210e
Dima <mdmitry2002@gmail.com>
2025-09-16 14:05:36 +0300

    feat(Infrastructure): add IdentityEntityConfiguration for ApplicationUser

---
+++ src/backend/FitCoachPro.Infrastructure/Identity/IdentityEntityConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using FitCoachPro.Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCoachPro.Infrastructure.Persistence.EntityConfigurations
+{
+    public class IdentityEntityConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.UserName).
+                IsRequired();
+            builder.HasIndex(x => x.UserName)
+                .IsUnique();
+
+            builder.Property(x => x.Email)
+                .IsRequired();
+            builder.HasIndex(x =>x.Email)
+                .IsUnique();
+
+            builder.Property(x => x.PhoneNumber);
+            builder.HasIndex(x => x.PhoneNumber)
+                .IsUnique();
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired();
+
+            builder.HasOne(x => x.DomainUser)
+                .WithOne()
+                .HasForeignKey<ApplicationUser>(x => x.DomainUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
