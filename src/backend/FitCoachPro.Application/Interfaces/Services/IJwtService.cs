7b91c251cdecd9203f7bf8e41b759afd3ba80774
Dima <mdmitry2002@gmail.com>
2025-09-25 23:47:51 +0300

    feat(Application): add JWtService interface

---
+++ src/backend/FitCoachPro.Application/Interfaces/Services/IJwtService.cs
@@ -0,0 +1,15 @@
+﻿using FitCoachPro.Application.Common.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace FitCoachPro.Application.Interfaces.Services
+{
+    public interface IJwtService
+    {
+        public string GenerateJWT(JwtUserModel jwtUserModel);
+    }
+}
