5f90a83370682cb051685430c8ee9ee7ca5770a3
Dima <mdmitry2002@gmail.com>
2025-09-25 23:43:38 +0300

    feat(Infrastructure): configure Identity, roles, and JWT authentication

--- src/backend/FitCoachPro.Infrastructure/InfrastructureExtensions.cs
+++ src/backend/FitCoachPro.Infrastructure/InfrastructureExtensions.cs
@@ -1,7 +1,12 @@
-﻿using FitCoachPro.Infrastructure.Persistence;
+﻿using FitCoachPro.Infrastructure.Identity;
+using FitCoachPro.Infrastructure.Persistence;
+using FitCoachPro.Infrastructure.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +19,47 @@ namespace FitCoachPro.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            //AddDb
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            //Identity
+            services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddRoles<IdentityRole<Guid>>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            // JwtOptions
+            services.Configure<JwtOptions>(configuration.GetSection("JwtOptions"));
+
+            // Authentication + JWT
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JwtOptions:Issuer"],
+                    ValidAudience = configuration["JwtOptions:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey
+                        (Encoding.UTF8.GetBytes(configuration["JwtOptions:Key"]!))
+                };
             });
 
             return services;
