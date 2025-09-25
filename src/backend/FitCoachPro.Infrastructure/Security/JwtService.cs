fe8262ccf1757767f226ce4fdd454e439d729597
Dima <mdmitry2002@gmail.com>
2025-09-25 23:47:20 +0300

    feat(Infrastructure): add JwtService and JwtOptions class

---
+++ src/backend/FitCoachPro.Infrastructure/Security/JwtService.cs
@@ -0,0 +1,43 @@
+﻿using FitCoachPro.Application.Common.Models.Users;
+using FitCoachPro.Application.Interfaces.Services;
+using FitCoachPro.Infrastructure.Security;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace FitCoachPro.Infrastructure.Services
+{
+    public class JwtService(IOptions<JwtOptions> jwtOptions) : IJwtService
+    {
+        private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+
+        public string GenerateJWT(JwtUserModel jwtUserModel)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Sub, jwtUserModel.Id.ToString()),
+                new Claim(ClaimTypes.Name, jwtUserModel.UserName),
+                new Claim(ClaimTypes.Role, jwtUserModel.Role.ToString())
+            };
+
+            var securityKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtOptions.Key));
+
+            var credentials = new SigningCredentials(
+                securityKey,
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddSeconds(_jwtOptions.ExpirationSeconds),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
