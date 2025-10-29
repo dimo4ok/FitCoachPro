1849cd140eaa2062ae9d05836ead5ad48290459d
Dima <mdmitry2002@gmail.com>
2025-10-29 17:58:59 +0200

    feat(Infrastructure/Application): add AppUser service and interface

---
+++ src/backend/FitCoachPro.Application/Interfaces/Services/IAppUserService.cs
@@ -0,0 +1,12 @@
+﻿using FitCoachPro.Application.Common.Models.Response;
+using FitCoachPro.Application.Common.Models.Users;
+using FitCoachPro.Domain.Entities.Users;
+
+namespace FitCoachPro.Application.Interfaces.Services
+{
+    public interface IAppUserService
+    {
+        Task<Result<bool>> CreateUserAsync(User domainUser, CreateUserModel model);
+        Task<Result<AppUserModel>> AuthenticateUserAsync(LoginUserModel model, CancellationToken cancellationToken = default);
+    }
+}
