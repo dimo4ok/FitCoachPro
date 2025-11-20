39874ab72dadb08681cdc3616ed91f1103ee69af
Dima <mdmitry2002@gmail.com>
2025-11-20 19:04:24 +0200

    feat(API): add GlobalExceptionHandler

---
+++ src/backend/FitCoachPro.API/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using FitCoachPro.Application.Common.Extensions;
+using FitCoachPro.Application.Common.Response;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json;
+
+namespace FitCoachPro.API.Exceptions;
+
+internal sealed class GlobalExceptionHandler
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalExceptionHandler(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        var result = Result.Fail(ex.ToError(), 500);
+
+        context.Response.StatusCode = result.StatusCode;
+
+        var json = JsonSerializer.Serialize(result, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        });
+
+        await context.Response.WriteAsync(json);
+    }
+}
