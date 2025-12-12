c63d950f4c444dbbe9d888460c3a913ad4bb3c2d
Dmytrii Makota <mdmitry2002@gmail.com>
2025-12-12 21:23:15 +0200

    feat(Applciation): add Mediator pattern implementation

---
+++ src/backend/FitCoachPro.Application/Mediator/Interfaces/ICommandHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace FitCoachPro.Application.Mediator.Interfaces;
+
+public interface ICommandHandler<in T>
+{
+    Task ExecuteAsync(T command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in T, TResult>
+{
+    Task<TResult> ExecuteAsync(T command, CancellationToken cancellationToken);
+}
---
+++ src/backend/FitCoachPro.Application/Mediator/Mediator.cs
@@ -0,0 +1,39 @@
+﻿using FitCoachPro.Application.Mediator.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitCoachPro.Application.Mediator;
+
+public class Mediator(IServiceScopeFactory scopeFactory) : IMediator
+{
+    public async Task ExecuteCommandAsync<T>(T command, CancellationToken cancellationToken)
+    {
+        await using var scope = scopeFactory.CreateAsyncScope();
+        var commandHandler = scope.ServiceProvider.GetRequiredService<ICommandHandler<T>>();
+
+        await commandHandler.ExecuteAsync(command, cancellationToken);
+    }
+
+    public async Task<TResult> ExecuteCommandAsync<T, TResult>(T command, CancellationToken cancellationToken)
+    {
+        await using var scope = scopeFactory.CreateAsyncScope();
+        var commandHandler = scope.ServiceProvider.GetRequiredService<ICommandHandler<T, TResult>>();
+
+        return await commandHandler.ExecuteAsync(command, cancellationToken);
+    }
+
+    public async Task ExecuteQueryAsync<T>(T query, CancellationToken cancellationToken)
+    {
+        await using var scope = scopeFactory.CreateAsyncScope();
+        var queryHandler = scope.ServiceProvider.GetRequiredService<IQueryHandler<T>>();
+
+        await queryHandler.ExecuteAsync(query, cancellationToken);
+    }
+
+    public async Task<TResult> ExecuteQueryAsync<T, TResult>(T query, CancellationToken cancellationToken)
+    {
+        await using var scope = scopeFactory.CreateAsyncScope();
+        var queryHandler = scope.ServiceProvider.GetRequiredService<IQueryHandler<T, TResult>>();
+
+        return await queryHandler.ExecuteAsync(query, cancellationToken);
+    }
+}
