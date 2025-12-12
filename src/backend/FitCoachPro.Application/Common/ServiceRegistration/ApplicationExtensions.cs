4604c97028757218e14a14e6f3ec3f1cab968971
Dmytrii Makota <mdmitry2002@gmail.com>
2025-12-12 21:25:46 +0200

    chore(Application): register Mediator, helpers, and access services

---
+++ src/backend/FitCoachPro.Application/Common/ServiceRegistration/ApplicationExtensions.cs
@@ -0,0 +1,41 @@
+﻿using FitCoachPro.Application.Common.Validators.WorkoutPlanValidators;
+using FitCoachPro.Application.Helpers;
+using FitCoachPro.Application.Interfaces.Helpers;
+using FitCoachPro.Application.Interfaces.Services;
+using FitCoachPro.Application.Interfaces.Services.Access;
+using FitCoachPro.Application.Mediator.Interfaces;
+using FitCoachPro.Application.Services;
+using FitCoachPro.Application.Services.Access;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitCoachPro.Application.Common.ServiceRegistration;
+
+public static class ApplicationExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<IMediator, Mediator.Mediator>().AddMediatorHandlers();
+
+        //helpers
+        services.AddScoped<IAuthHelper, AuthHelper>();
+        services.AddScoped<IWorkoutPlanHelper, WorkoutPlanHelper>();
+        services.AddScoped<ITemplateWorkoutPlanHelper, TemplateWorkoutPlanHelper>();
+        services.AddScoped<IUserHelper, UserHelper>();
+
+        //services
+        services.AddScoped<IUserContextService, UserContextService>();
+
+        services.AddScoped<IWorkoutPlanAccessService, WorkoutPlanAccessService>();
+        services.AddScoped<ITemplateWorkoutPlanAccessService, TemplateWorkoutPlanAccessService>();
+        services.AddScoped<IExerciseAccessService, ExerciseAccessService>();
+        services.AddScoped<IClientCoachRequestAccessService, ClientCoachRequestAccessService>();
+
+        //validator
+        services.AddValidatorsFromAssembly(typeof(CreateWorkoutPlanModelValidator).Assembly);
+
+        services.AddHttpContextAccessor();
+
+        return services;
+    }
+}
---
+++ src/backend/FitCoachPro.Application/Common/ServiceRegistration/MediatorHandlerExtensions.cs
@@ -0,0 +1,93 @@
+﻿using FitCoachPro.Application.Commands.Auth.SignIn;
+using FitCoachPro.Application.Commands.Auth.SignUp;
+using FitCoachPro.Application.Commands.ClientCoachRequests.CancelClientCoachRequest;
+using FitCoachPro.Application.Commands.ClientCoachRequests.CreateClientCoachRequest;
+using FitCoachPro.Application.Commands.ClientCoachRequests.DeleteOwnClientCoachRequests;
+using FitCoachPro.Application.Commands.ClientCoachRequests.UpdateClientCoachRequest;
+using FitCoachPro.Application.Commands.Exercsies.CreateExercise;
+using FitCoachPro.Application.Commands.Exercsies.DeleteExercise;
+using FitCoachPro.Application.Commands.Exercsies.UpdateExercise;
+using FitCoachPro.Application.Commands.TemplateWorkoutPlans.CreateTemplateWorkoutPlan;
+using FitCoachPro.Application.Commands.TemplateWorkoutPlans.DeleteTemplateWorkoutPlan;
+using FitCoachPro.Application.Commands.TemplateWorkoutPlans.UpdateTemplateWorkoutPlan;
+using FitCoachPro.Application.Commands.WorkoutPlans.CreateWorkoutPlan;
+using FitCoachPro.Application.Commands.WorkoutPlans.DeleteWorkoutPlan;
+using FitCoachPro.Application.Commands.WorkoutPlans.UpdateWorkoutPlan;
+using FitCoachPro.Application.Common.Models.Auth;
+using FitCoachPro.Application.Common.Models.Pagination;
+using FitCoachPro.Application.Common.Models.Requests;
+using FitCoachPro.Application.Common.Models.Workouts.Exercise;
+using FitCoachPro.Application.Common.Models.Workouts.TemplateWorkoutPlan;
+using FitCoachPro.Application.Common.Models.Workouts.WorkoutPlan;
+using FitCoachPro.Application.Common.Response;
+using FitCoachPro.Application.Mediator.Interfaces;
+using FitCoachPro.Application.Queries.ClientCoachRequests.GetAllClientCoachRequestsForCoachOrClient;
+using FitCoachPro.Application.Queries.ClientCoachRequests.GetAllForAdmin;
+using FitCoachPro.Application.Queries.ClientCoachRequests.GetClientCoachRequestById;
+using FitCoachPro.Application.Queries.Exercsies.GetAllExercises;
+using FitCoachPro.Application.Queries.Exercsies.GetExerciseById;
+using FitCoachPro.Application.Queries.TemplateWorkoutPlans.GetAllTempalatesForAdminByCoachId;
+using FitCoachPro.Application.Queries.TemplateWorkoutPlans.GetAllTemplatesForCoach;
+using FitCoachPro.Application.Queries.TemplateWorkoutPlans.GetTemplateById;
+using FitCoachPro.Application.Queries.WorkoutPlans.GetClientWorkoutPlans;
+using FitCoachPro.Application.Queries.WorkoutPlans.GetMyWorkoutPlans;
+using FitCoachPro.Application.Queries.WorkoutPlans.GetWorkoutPlanById;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FitCoachPro.Application.Common.ServiceRegistration;
+
+public static class MediatorHandlerExtensions
+{
+    public static IServiceCollection AddMediatorHandlers(this IServiceCollection services)
+    {
+        //auth
+        services.AddScoped<ICommandHandler<SignInCommand, Result<AuthModel>>, SignInCommandHandler>();
+        services.AddScoped<ICommandHandler<SignUpCommand, Result<AuthModel>>, SignUpCommandHandler>();
+
+        //wokroutPlans
+        services.AddScoped<IQueryHandler<GetWorkoutPlanByIdQuery, Result<WorkoutPlanModel>>, GetWorkoutPlanByIdQueryHandler>();
+        services.AddScoped<IQueryHandler<GetClientWorkoutPlansQuery, Result<PaginatedModel<WorkoutPlanModel>>>, GetClientWorkoutPlansQueryHandler>();
+        services.AddScoped<IQueryHandler<GetMyWorkoutPlansQuery, Result<PaginatedModel<WorkoutPlanModel>>>, GetMyWorkoutPlansQueryHandler>();
+
+        services.AddScoped<ICommandHandler<CreateWorkoutPlanCommand, Result>, CreateWorkoutPlanCommandHandler>();
+        services.AddScoped<ICommandHandler<UpdateWorkoutPlanCommand, Result>, UpdateWorkoutPlanCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteWorkoutPlanCommand, Result>, DeleteWorkoutPlanCommandHandler>();
+
+        //templateWorkoutPlans
+        services.AddScoped<IQueryHandler<GetTemplateByIdQuery, Result<TemplateWorkoutPlanModel>>, GetTemplateByIdQueryHandler>();
+        services.AddScoped<IQueryHandler<GetAllTemplatesForCoachQuery, Result<PaginatedModel<TemplateWorkoutPlanModel>>>, GetAllTemplatesForCoachQueryHandler>();
+        services.AddScoped<
+            IQueryHandler<GetAllTempalatesForAdminByCoachIdQuery, Result<PaginatedModel<TemplateWorkoutPlanModel>>>,
+            GetAllTempalatesForAdminByCoachIdQueryHandler>();
+
+        services.AddScoped<ICommandHandler<CreateTemplateCommand, Result>, CreateTemplateCommandHandler>();
+        services.AddScoped<ICommandHandler<UpdateTemplateCommand, Result>, UpdateTemplateCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteTemplateCommand, Result>, DeleteTemplateCommandHandler>();
+
+        //exercises
+        services.AddScoped<IQueryHandler<GetExerciseByIdQuery, Result<ExerciseDetailModel>>, GetExerciseByIdQueryHandler>();
+        services.AddScoped<IQueryHandler<GetAllExercisesQuery, Result<PaginatedModel<ExerciseDetailModel>>>, GetAllExercisesQueryHandler>();
+
+        services.AddScoped<ICommandHandler<CreateExerciseCommand, Result>, CreateExerciseCommandHandler>();
+        services.AddScoped<ICommandHandler<UpdateExerciseCommand, Result>, UpdateExerciseCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteExerciseCommand, Result>, DeleteExerciseCommandHandler>();
+
+        //coachClientRequests
+        services.AddScoped<
+            IQueryHandler<GetClientCoachRequestByIdQuery, Result<ClientCoachRequestModel>>,
+            GetClientCoachRequestByIdQueryHandler>();
+        services.AddScoped<
+            IQueryHandler<GetAllClientCoachRequestsForAdminQuery, Result<PaginatedModel<ClientCoachRequestModel>>>,
+            GetAllClientCoachRequestsForAdminQueryHandler>();
+        services.AddScoped<
+            IQueryHandler<GetAllClientCoachRequestsForCoachOrClientQuery, Result<PaginatedModel<ClientCoachRequestModel>>>,
+            GetAllClientCoachRequestsForCoachOrClientQueryHandler>();
+
+        services.AddScoped<ICommandHandler<CreateClientCoachRequestCommand, Result>, CreateClientCoachRequestCommandHandler>();
+        services.AddScoped<ICommandHandler<UpdateClientCoachRequestCommand, Result>, UpdateClientCoachRequestCommandHandler>();
+        services.AddScoped<ICommandHandler<CancelClientCoachRequestCommand, Result>, CancelClientCoachRequestCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteOwnClientCoachRequestsCommand, Result>, DeleteOwnClientCoachRequestsCommandHandler>();
+
+        return services;
+    }
+}
