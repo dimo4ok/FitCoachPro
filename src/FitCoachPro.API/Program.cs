63f5e4b58fbeeda3793bed682f8200d39ea7aac8
Dmytrii Makota <mdmitry2002@gmail.com>
2026-02-23 16:35:10 +0200

    feat(API): add swagger as a default page in production

--- src/FitCoachPro.API/Program.cs
+++ src/FitCoachPro.API/Program.cs
@@ -36,9 +36,18 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+if (app.Environment.IsProduction())
 {
-    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "FitCoachPro API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+else
+{
     app.UseSwaggerUI();
 }
 
